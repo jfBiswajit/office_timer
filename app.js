1403e85be01430fb4f80516b125085666a6e9a71
Biswajit Biswas <jfbiswajit@gmail.com>
2021-05-31 09:26:57 +0600

    Add: new font added

--- app.js
+++ app.js
@@ -22,6 +22,18 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  
+  if (minutes < 10) {
+    minutes = '0' + hours;
+  }
+  
+  if (seconds < 10) {
+    seconds = '0' + hours;
+  }
     
   document.getElementById('show_time').innerHTML = `${hours}h:${minutes}m:${seconds}s`;
     
