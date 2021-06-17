ee743bd8c0b2b8ffb183ff75a9bf3a40d76897d7
Biswajit Biswas <jfbiswajit@gmail.com>
2021-06-17 13:59:21 +0600

    Ref: refactor some portion

--- app.js
+++ app.js
@@ -1,50 +1,60 @@
 const addHoursToDate = (date, minutesToAdd) => {
   return (futureDate = new Date(date.getTime() + minutesToAdd * 60000));
-}
+};
 
+const SetTimmer = () => {
+  let officeHoursInMinute = 525;
+  let startWorkingAt = '08:56:05';
 
-let officeHoursInMinute = 525;
-let startWorkingAt = '08:56:05';
+  let today = new Date();
+  today =
+    today.getMonth() + 1 + ' ' + today.getDate() + ' ' + today.getFullYear();
 
-let today = new Date();
-today =
-  today.getMonth() + 1 + ' ' + today.getDate() + ' ' + today.getFullYear();
+  let startedAt = new Date(`${today} ${startWorkingAt} GMT+0600`);
+  let endAt = addHoursToDate(startedAt, officeHoursInMinute);
 
-let startedAt = new Date(`${today} ${startWorkingAt} GMT+0600`);
-let endAt = addHoursToDate(startedAt, officeHoursInMinute);
+  let countDownDate = new Date('Jan 5, 2022 15:37:25').getTime();
 
-let countDownDate = new Date('Jan 5, 2022 15:37:25').getTime();
+  let clock = setInterval(function () {
+    let now = new Date().getTime();
+    let distance = endAt - now;
 
-let clock = setInterval(function () {
-  let now = new Date().getTime();
-  let distance = endAt - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
 
-  if (hours < 10) {
-    hours = '0' + hours;
-  }
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
 
-  if (minutes < 10) {
-    minutes = '0' + minutes;
-  }
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
 
-  if (seconds < 10) {
-    seconds = '0' + seconds;
-  }
-
-  document.getElementById(
-    'show_time'
-  ).innerHTML = `${hours}h:${minutes}m:${seconds}s`;
-
-  if (distance < 0) {
-    clearInterval(clock);
-    console.log('Experied');
     document.getElementById(
       'show_time'
-    ).innerHTML = `00h:00m:00s`;
-  }
-}, 1000);
+    ).innerHTML = `${hours}h:${minutes}m:${seconds}s`;
+
+    if (distance < 0) {
+      clearInterval(clock);
+      console.log('Experied');
+      document.getElementById('show_time').innerHTML = `00h:00m:00s`;
+    }
+  }, 1000);
+};
+
+const app = {
+  init: function () {
+    SetTimmer();
+  },
+};
+
+
+app.init();
